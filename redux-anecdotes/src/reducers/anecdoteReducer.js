d7002bedfddebd70831c3ce131c3a439843ae2b7
SHI ZHEN <shi.zhen@vincit.fi>
2020-10-27 17:55:41 +0200

    feat(6.3): Vote function

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,18 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  return state
+  console.log(action)
+  switch (action.type) {
+    case 'VOTE':
+      return state.map((anecdote) => anecdote.id === action.data.id ? { ...anecdote, votes: anecdote.votes + 1 } : anecdote)
+    default: return state
+  }
 }
 
+export const incrementVote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
+}
 export default reducer
