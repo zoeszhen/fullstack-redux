cd9fdcb2673cdd123519e53fb24c04ff44728a1e
SHI ZHEN <shi.zhen@vincit.fi>
2020-10-29 11:22:49 +0200

    feat(6.14): Add new to backend

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
-import Notification from "./Notification"
+import anecdoteService from "../service/anecdotes"
 
 const AnecdoteForm = (props) => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async(event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ""
-        dispatch(createAnecdote(content))
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(createAnecdote(newAnecdote))
         dispatch(setNotification("creat new anecdote"))
         setTimeout(() => {
             dispatch(setNotification(null))
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -45,11 +45,7 @@ export const incrementVote = (id) => {
 export const createAnecdote = (content) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: {
-      content,
-      votes: 0,
-      id: getId()
-    }
+    data: content
   }
 }
 export const initAnecdote = (anecdoteList) => {
